36afff545039e33da494233a8406be3b40f82790
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2026-04-03 11:29:24 -0700

    Use of modern Java features and general code cleanup (#102)
    Signed-off-by: BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>

--- platform/platform-modern/src/main/java/dev/pgm/community/platform/modern/ModernPlayerUtils.java
+++ platform/platform-modern/src/main/java/dev/pgm/community/platform/modern/ModernPlayerUtils.java
@@ -6,7 +6,6 @@ import com.destroystokyo.paper.profile.CraftPlayerProfile;
 import com.destroystokyo.paper.profile.PlayerProfile;
 import dev.pgm.community.util.PlayerUtils;
 import dev.pgm.community.util.Supports;
-import dev.pgm.community.utils.MessageUtils;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -14,6 +13,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.Player;
@@ -22,10 +22,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.profile.PlayerTextures;
 import tc.oc.pgm.platform.modern.util.Skins;
-import tc.oc.pgm.util.bukkit.BukkitUtils;
 import tc.oc.pgm.util.skin.Skin;
 
-@Supports(value = PAPER, minVersion = "1.20.6")
+@Supports(value = PAPER, minVersion = "1.21.11")
 public class ModernPlayerUtils implements PlayerUtils {
   @Override
   public Skin getPlayerSkin(Player player) {
@@ -56,9 +55,10 @@ public class ModernPlayerUtils implements PlayerUtils {
     if (playerDisplayNames.containsKey(player.getUniqueId())) {
       Map<UUID, String> uuidStringMap = playerDisplayNames.get(player.getUniqueId());
       String displayName = uuidStringMap.get(viewer.getUniqueId());
-      return displayName == null ? player.getDisplayName() : displayName;
+      if (displayName != null) return displayName;
     }
-    return player.getDisplayName();
+
+    return LegacyComponentSerializer.legacySection().serialize(player.displayName());
   }
 
   @Override
@@ -66,23 +66,21 @@ public class ModernPlayerUtils implements PlayerUtils {
     if (playerNames.containsKey(player.getUniqueId())) {
       Map<UUID, String> uuidStringMap = playerNames.get(player.getUniqueId());
       String name = uuidStringMap.get(viewer.getUniqueId());
-      return name == null ? player.getName() : name;
+      if (name != null) return name;
     }
+
     return player.getName();
   }
 
   @Override
   public Skin getPlayerSkin(Player player, Player viewer) {
-    return null;
-    //    if (playerSkins.containsKey(player.getUniqueId())) {
-    //      Map<UUID, Skin> uuidSkinMap = playerSkins.get(player.getUniqueId());
-    //      Skin skin = uuidSkinMap.get(viewer.getUniqueId());
-    //      if (skin == null) {
-    //        return new Skin(player.getPlayerProfile().getTextures())
-    //      }
-    //      return skin == null ? Skin.EMPTY : skin;
-    //    }
-    //    return Skin.EMPTY;
+    if (playerSkins.containsKey(player.getUniqueId())) {
+      Map<UUID, Skin> uuidSkinMap = playerSkins.get(player.getUniqueId());
+      Skin skin = uuidSkinMap.get(viewer.getUniqueId());
+      if (skin != null) return skin;
+    }
+
+    return getPlayerSkin(player);
   }
 
   @Override
@@ -104,8 +102,13 @@ public class ModernPlayerUtils implements PlayerUtils {
       e.printStackTrace();
     }
 
-    headMeta.setDisplayName(BukkitUtils.colorize(displayName));
-    headMeta.setLore(MessageUtils.colorizeList(Arrays.asList(lore)));
+    var displayNameComponent = LegacyComponentSerializer.legacyAmpersand().deserialize(displayName);
+    var loreComponent = Arrays.stream(lore)
+        .map(each -> LegacyComponentSerializer.legacyAmpersand().deserialize(each))
+        .toList();
+
+    headMeta.displayName(displayNameComponent);
+    headMeta.lore(loreComponent);
     headMeta.addItemFlags(ItemFlag.values());
     head.setItemMeta(headMeta);
     return head;
